b225501b1f75f7af0b240284f99dccae23dff09f
anrachana28-del <anrachana28@gmail.com>
2026-05-09 08:09:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express=require("express");
 const multer=require("multer");
 const axios=require("axios");
 const fs=require("fs");
-const path=require("path");
 
 const app=express();
 const upload=multer({dest:"/tmp"});
 
-// upload video → python AI
 app.post("/upload-video",upload.single("file"),async(req,res)=>{
 
     const file=fs.readFileSync(req.file.path);
 
     try{
+
         const result=await axios.post(
-            "https://YOUR-PYTHON-URL/video-dub",
+            "https://ai-ouub.onrender.com/video-dub",
             file,
             {
                 headers:{
@@ -26,13 +25,17 @@ app.post("/upload-video",upload.single("file"),async(req,res)=>{
         res.json(result.data);
 
     }catch(err){
-        res.status(500).json({error:err.message});
+
+        res.status(500).json({
+            error:err.message
+        });
+
     }
+
 });
 
-// serve generated videos
 app.use("/files",express.static("/tmp"));
 
 app.listen(3000,()=>{
-    console.log("Node running on 3000");
+    console.log("Node running");
 });
